3a745bd7d9f122ee2e1b323dae623c5d8785cb84
economicdonut <143364762+economicdonut@users.noreply.github.com>
2023-10-15 17:04:07 +0800

    added profileinfo

---
+++ src/components/Account/ProfileInfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <img src="" alt="">
+        </div>
+        <div class="row">
+            <p>username</p>
+        </div>
+        <div class="row">
+            <p>level and exp bar</p>
+        </div>
+        <div class="row">
+            <p>ex to next level</p>
+        </div>
+    </div>
+        
+    </template>
+    
+    <script>
+        export default {
+            name: 'ProfileInfo',
+            components: {
+    
+            }
+        }
+    </script>
+    
+    <style scoped>
+    .container{
+        margin-left: auto;
+        margin-right: auto;
+        background-color: lightgray;
+    }
+    p {
+        text-align: center;
+    }
+    img {
+      border-radius: 50%;
+    }
+    
+    </style>
+    
