d882c4fb0015cca1ec6b572d5f61322804fbce49
ryanang333 <ryanang333@gmail.com>
2023-11-02 21:30:25 +0800

    deployment stuff

--- src/components/Game/TopicList.vue
+++ src/components/Game/TopicList.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-12 text-center mb-5 mt-5">
           <img
-            src="src\assets\images\START-YOUR-CODEQUEST.png"
+            src="/src/assets/images/START-YOUR-CODEQUEST.png"
             class="img-fluid mt-5"
           />
         </div>
