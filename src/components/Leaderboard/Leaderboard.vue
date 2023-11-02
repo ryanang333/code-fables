d62f8c93d848b477846e2da9180bd115aace9b17
ryanang333 <ryanang333@gmail.com>
2023-11-02 17:30:57 +0800

    made changes to navbar

--- src/components/Leaderboard/Leaderboard.vue
+++ src/components/Leaderboard/Leaderboard.vue
@@ -1,132 +1,194 @@
 <template>
   <div class="bg-image p-4">
-    <div class="bg-overlay p-5">
-           
-  </div>
+    <div class="bg-overlay p-5"></div>
 
-  <div class="container ">
-        <div
-          class="container" style="position: fixed; left: 0px; top: 80px; z-index: 999;"
-        >
-          <button
-            @click="friendBool"
-            class="btn btn-lg btn-success top"
-            id="nav-friends-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#nav-friends"
-            type="button"
-            role="tab"
-            aria-controls="nav-friends"
-            aria-selected="true"
-            v-if="friendsBool == true"
-          >
-            Switch to Global
-          </button>
-          <button
-            @click="friendBool"
-            class="btn btn-lg btn-success top"
-            id="nav-leaderboard-tab"
-            data-bs-toggle="tab"
-            data-bs-target="#nav-leaderboard"
-            type="button"
-            role="tab"
-            aria-controls="nav-leaderboard"
-            aria-selected="false"
-            v-if="friendsBool == false"
-          >
-            Switch to Friends
-          </button>
-        </div>
-      </div>
-  
-  <div class="container mt-5 podium-div" v-if="leaderboardPodium.length == 3">
-    
-    <h2 class="text-center"><img class="w-100" src="/src/assets/images/global-leaderboard.png" v-if="friendsBool == false"></h2>
-    <div class="mt-4 row justify-content-center">
-      <div class="col-lg-3 col-md-12 order-lg-1 order-2 mt-5 text-center">
-        <img class="podium" src="/src/assets/images/king.png" />
-        <div
-          class=" card podiumBack shadow d-flex flex-direction-column justify-content-center align-items-center" v-bind:style="{ backgroundColor: leaderboardPodium[1].email==this.myUser ? 'lightgoldenrodyellow' : '#C0C0C0' , borderColor : leaderboardPodium[1].email==this.myUser ? '#00AEEF ' : '#7e6e5c' , boxShadow : leaderboardPodium[1].email==this.myUser ?  '0 0 30px 50px #48abe0':'' }"
-        >
-          <img
-            class="mt-4 podium mb-2"
-            :src="leaderboardPodium[1].profile_pic_ID"
-          />
-          <h3 class="fw-bold">{{ leaderboardPodium[1].profile_name }}</h3>
-          <p class="fw-bold">Level {{ leaderboardPodium[1].level }}</p>
-          <p class="">EXP: {{ leaderboardPodium[1].exp }}</p>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-12 order-lg-2 order-1 text-center podium-div">
-        <img class="podium" src="/src/assets/images/premium.png" />
-        <div
-          class=" card podiumBack shadow d-flex flex-direction-column justify-content-center align-items-center" v-bind:style="{ backgroundColor: leaderboardPodium[0].email==this.myUser ? 'lightgoldenrodyellow' : '#FFD700' , borderColor : leaderboardPodium[0].email==this.myUser ? '#00AEEF ' : '#7e6e5c' , boxShadow : leaderboardPodium[0].email==this.myUser ?  '0 0 30px 50px #48abe0':'' }"
-        >
-          <img
-            class="mt-4 podium mb-2"
-            :src="leaderboardPodium[0].profile_pic_ID"
-          />
-          <h3 class="fw-bold">{{ leaderboardPodium[0].profile_name }}</h3>
-          <p class="fw-bold">Level {{ leaderboardPodium[0].level }}</p>
-          <p>EXP: {{ leaderboardPodium[0].exp }}</p>
-
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-12 order-lg-3 order-3 mt-5 text-center podium-div">
-        <img class="podium" src="/src/assets/images/crown.png" />
-        <div
-          class=" card podiumBack shadow d-flex flex-direction-column justify-content-center align-items-center"  v-bind:style="{ backgroundColor: leaderboardPodium[2].email==this.myUser ? 'lightgoldenrodyellow' : '#B87333' , borderColor : leaderboardPodium[2].email==this.myUser ? '#00AEEF ' : '#7e6e5c' , boxShadow : leaderboardPodium[2].email==this.myUser ?  '0 0 30px 50px #48abe0':'' }"
-        >
-          <img
-            class="mt-4 podium mb-2"
-            :src="leaderboardPodium[2].profile_pic_ID"
-          />
-          <h3 class="fw-bold">{{ leaderboardPodium[2].profile_name }}</h3>
-          <p class="fw-bold">Level {{ leaderboardPodium[2].level }}</p>
-          <p>EXP: {{ leaderboardPodium[2].exp }}</p>
-   
-        </div>
-      </div>
-    </div>
-    <br />
-    <hr />
-  </div>
-  <div class="container mt-5">
-    <h2 class="text-center"><img class="w-100" src="/src/assets/images/friends-leaderboard.png" v-if="friendsBool == true"></h2>
-    <div
-      class="padding-5"
-      v-for="(person, index) in leaderboardList"
-      :key="person.profile_name"
-    >
+    <div class="container">
       <div
-        class="row listItem d-flex mb-4 align-items-center rounded-3 px-3 pt-3 d-flex-row" v-bind:style="{ backgroundColor: person.email==this.myUser ? 'lightgoldenrodyellow' : '#cdbea2' , borderColor : person.email==this.myUser ? '#00AEEF ' : '#7e6e5c', boxShadow : person.email==this.myUser ?  '0 0 30px 5px #48abe0':''}"
+        class="container"
+        style="position: fixed; left: 0px; top: 80px; z-index: 999"
       >
-        <div class="col-md-1 mb-3 me-4">
-          <h2 v-if="friendsBool == true">{{ index + 1 }}</h2>
-          <h2 v-else>{{ index + 4 }}</h2>
+        <button
+          @click="friendBool"
+          class="btn btn-lg btn-success top"
+          id="nav-friends-tab"
+          data-bs-toggle="tab"
+          data-bs-target="#nav-friends"
+          type="button"
+          role="tab"
+          aria-controls="nav-friends"
+          aria-selected="true"
+          v-if="friendsBool == true"
+        >
+          Switch to Global
+        </button>
+        <button
+          @click="friendBool"
+          class="btn btn-lg btn-success top"
+          id="nav-leaderboard-tab"
+          data-bs-toggle="tab"
+          data-bs-target="#nav-leaderboard"
+          type="button"
+          role="tab"
+          aria-controls="nav-leaderboard"
+          aria-selected="false"
+          v-if="friendsBool == false"
+        >
+          Switch to Friends
+        </button>
+      </div>
+    </div>
+
+    <div class="container mt-5 podium-div" v-if="leaderboardPodium.length == 3">
+      <h2 class="text-center">
+        <img
+          class="w-100"
+          src="/src/assets/images/global-leaderboard.png"
+          v-if="friendsBool == false"
+        />
+      </h2>
+      <div class="mt-4 row justify-content-center">
+        <div class="col-lg-3 col-md-12 order-lg-1 order-2 mt-5 text-center">
+          <img class="podium" src="/src/assets/images/king.png" />
+          <div
+            class="card podiumBack shadow d-flex flex-direction-column justify-content-center align-items-center"
+            v-bind:style="{
+              backgroundColor:
+                leaderboardPodium[1].email == this.myUser
+                  ? 'lightgoldenrodyellow'
+                  : '#C0C0C0',
+              borderColor:
+                leaderboardPodium[1].email == this.myUser
+                  ? '#00AEEF '
+                  : '#7e6e5c',
+              boxShadow:
+                leaderboardPodium[1].email == this.myUser
+                  ? '0 0 30px 50px #48abe0'
+                  : '',
+            }"
+          >
+            <img
+              class="mt-4 podium mb-2"
+              :src="leaderboardPodium[1].profile_pic_ID"
+            />
+            <h3 class="fw-bold">{{ leaderboardPodium[1].profile_name }}</h3>
+            <p class="fw-bold">Level {{ leaderboardPodium[1].level }}</p>
+            <p class="">EXP: {{ leaderboardPodium[1].exp }}</p>
+          </div>
         </div>
         <div
-          class="col-md-2 justify-content-center d-flex flex-column align-items-center"
+          class="col-lg-3 col-md-12 order-lg-2 order-1 text-center podium-div"
         >
-          <img class="podium" :src="person.profile_pic_ID" />
-          <p class="fw-bold text-center">Level {{ person.level }}</p>
+          <img class="podium" src="/src/assets/images/premium.png" />
+          <div
+            class="card podiumBack shadow d-flex flex-direction-column justify-content-center align-items-center"
+            v-bind:style="{
+              backgroundColor:
+                leaderboardPodium[0].email == this.myUser
+                  ? 'lightgoldenrodyellow'
+                  : '#FFD700',
+              borderColor:
+                leaderboardPodium[0].email == this.myUser
+                  ? '#00AEEF '
+                  : '#7e6e5c',
+              boxShadow:
+                leaderboardPodium[0].email == this.myUser
+                  ? '0 0 30px 50px #48abe0'
+                  : '',
+            }"
+          >
+            <img
+              class="mt-4 podium mb-2"
+              :src="leaderboardPodium[0].profile_pic_ID"
+            />
+            <h3 class="fw-bold">{{ leaderboardPodium[0].profile_name }}</h3>
+            <p class="fw-bold">Level {{ leaderboardPodium[0].level }}</p>
+            <p>EXP: {{ leaderboardPodium[0].exp }}</p>
+          </div>
         </div>
-        <div class="col-md-6 mb-3 text-center">
-          <h2>{{ person.profile_name }}</h2>
+        <div
+          class="col-lg-3 col-md-12 order-lg-3 order-3 mt-5 text-center podium-div"
+        >
+          <img class="podium" src="/src/assets/images/crown.png" />
+          <div
+            class="card podiumBack shadow d-flex flex-direction-column justify-content-center align-items-center"
+            v-bind:style="{
+              backgroundColor:
+                leaderboardPodium[2].email == this.myUser
+                  ? 'lightgoldenrodyellow'
+                  : '#B87333',
+              borderColor:
+                leaderboardPodium[2].email == this.myUser
+                  ? '#00AEEF '
+                  : '#7e6e5c',
+              boxShadow:
+                leaderboardPodium[2].email == this.myUser
+                  ? '0 0 30px 50px #48abe0'
+                  : '',
+            }"
+          >
+            <img
+              class="mt-4 podium mb-2"
+              :src="leaderboardPodium[2].profile_pic_ID"
+            />
+            <h3 class="fw-bold">{{ leaderboardPodium[2].profile_name }}</h3>
+            <p class="fw-bold">Level {{ leaderboardPodium[2].level }}</p>
+            <p>EXP: {{ leaderboardPodium[2].exp }}</p>
+          </div>
         </div>
-        <div class="col mb-3">
-          <h3>{{ person.exp }}xp</h3>
+      </div>
+      <br />
+      <hr />
+    </div>
+    <div class="container mt-5">
+      <h2 class="text-center">
+        <img
+          class="w-100"
+          src="/src/assets/images/friends-leaderboard.png"
+          v-if="friendsBool == true"
+        />
+      </h2>
+      <div
+        class="padding-5"
+        v-for="(person, index) in leaderboardList"
+        :key="person.profile_name"
+      >
+        <div
+          class="row listItem d-flex mb-4 align-items-center rounded-3 px-3 pt-3 d-flex-row"
+          v-bind:style="{
+            backgroundColor:
+              person.email == this.myUser ? 'lightgoldenrodyellow' : '#cdbea2',
+            borderColor: person.email == this.myUser ? '#00AEEF ' : '#7e6e5c',
+            boxShadow:
+              person.email == this.myUser ? '0 0 30px 5px #48abe0' : '',
+          }"
+        >
+          <div class="col-md-1 mb-3 me-4">
+            <h2 v-if="friendsBool == true">{{ index + 1 }}</h2>
+            <h2 v-else>{{ index + 4 }}</h2>
+          </div>
+          <div
+            class="col-md-2 justify-content-center d-flex flex-column align-items-center"
+          >
+            <img class="podium" :src="person.profile_pic_ID" />
+            <p class="fw-bold text-center">Level {{ person.level }}</p>
+          </div>
+          <div class="col-md-6 mb-3 text-center">
+            <h2>{{ person.profile_name }}</h2>
+          </div>
+          <div class="col mb-3">
+            <h3>{{ person.exp }}xp</h3>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 import { getAuth } from "firebase/auth";
 import db from "/src/firebase/init";
-import { getDocs,
+import {
+  getDocs,
   query,
   collection,
   orderBy,
@@ -138,6 +200,7 @@ export default {
   components: {},
   data() {
     return {
+      noFriend: false,
       UID: "",
       myUser: "",
       myUsername: "",
@@ -148,15 +211,14 @@ export default {
   },
   methods: {
     async friendBool() {
-      if (this.friendsBool == true){
-        this.friendsBool = false
-        console.log(this.friendsBool)
-        this.getAllUsers()
-      }
-      else{
-        this.friendsBool = true
-        console.log(this.friendsBool)
-        this.getAllFriends()
+      if (this.friendsBool == true) {
+        this.friendsBool = false;
+        console.log(this.friendsBool);
+        this.getAllUsers();
+      } else {
+        this.friendsBool = true;
+        console.log(this.friendsBool);
+        this.getAllFriends();
       }
     },
     async getAllUsers() {
@@ -184,11 +246,10 @@ export default {
       const q = query(collection(db, "accounts"), orderBy("exp", "desc"));
       const querySnap = await getDocs(q);
       querySnap.forEach((doc) => {
-      if (friendsList.includes(doc.data().email)) {
+        if (friendsList.includes(doc.data().email)) {
           this.leaderboardList.push(doc.data());
-        }       
-  
-      })
+        }
+      });
     },
   },
   mounted() {
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -3,42 +3,60 @@
     <div class="container-fluid mx-5 row p-1">
       <div class="col-lg-1 col-2 mx-auto">
         <a class="navbar-brand" href="/">
-          <img src="/src/assets/images/code-fables.png" style="width:150px; height:auto;"/>
+          <img
+            src="/src/assets/images/code-fables.png"
+            style="width: 200px; height: auto"
+          />
         </a>
       </div>
-      
+
       <div class="col-lg-3 col-8"></div>
-        <div class="col-2 mx-auto">
-          <button
-            class="navbar-toggler bg-success"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-   
-      <div class="col-lg-4 col-sm-3 justify-content-end collapse navbar-collapse" id="navbarNav">
+      <div class="col-2 mx-auto">
+        <button
+          class="navbar-toggler bg-success"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div
+        class="col-lg-4 col-sm-3 justify-content-end collapse navbar-collapse"
+        id="navbarNav"
+      >
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link me-lg-2 me-xl-5 " aria-current="page" href="/leaderboard">Leaderboard</a>
+            <a
+              class="nav-link me-lg-2 me-xl-5"
+              aria-current="page"
+              href="/leaderboard"
+              >Leaderboard</a
+            >
           </li>
-          <li class="nav-item me-lg-2 me-xl-5 ">
-            <a class="nav-link " href="/myfriends">Friends</a>
+          <li class="nav-item me-lg-2 me-xl-5">
+            <a class="nav-link" href="/myfriends">Friends</a>
           </li>
           <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Account
-          </a>
-          <ul class="dropdown-menu bg-dark text-white border-0 ">
-            <li><a class="dropdown-item " href="/account">My Profile</a></li>
-            <li> <button class="dropdown-item " @click="logOut">Sign Out</button></li>
-          </ul>
-        </li>
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Account
+            </a>
+            <ul class="dropdown-menu bg-dark text-white border-0">
+              <li><a class="dropdown-item" href="/account">My Profile</a></li>
+              <li>
+                <button class="dropdown-item" @click="logOut">Sign Out</button>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
@@ -51,39 +69,35 @@ let auth;
 export default {
   name: "NavBar",
   data() {
-    return {
-      
-    }
+    return {};
   },
-  methods:{
-    logOut(){
+  methods: {
+    logOut() {
       let auth = getAuth();
       signOut(auth).then(() => {
-        this.$router.push('/login');
-      })
-    }
+        this.$router.push("/login");
+      });
+    },
   },
- 
 };
 </script>
 
-<style scoped> 
-#sticky-nav{
-  position:sticky ;
-  top:0;
-  z-index:100;
+<style scoped>
+#sticky-nav {
+  position: sticky;
+  top: 0;
+  z-index: 100;
 }
-a, button {
+a,
+button {
   color: white;
-  font-size: 18px;
-  text-decoration:none;
+  font-size: 22px;
+  text-decoration: none;
   font-family: Georgia, serif;
 }
 
 a:hover {
   opacity: 0.9; /* Decrease the opacity on hover */
-  color: #88fcd7
+  color: #88fcd7;
 }
-
-
 </style>
